a0f67f15b81ce3c47256dede7899aeafdec19b76
Raksa Adhinata <raksaadhinata@gmail.com>
2026-05-14 02:43:34 +0700

    Fix SQL formatting in gold_company_profiles table

--- src/gold.sql
+++ src/gold.sql
@@ -1,9 +1,8 @@
 CREATE OR REPLACE TABLE gold_company_profiles AS
 SELECT
-company_name AS perusahaan,
-primary_ticker AS kode_saham,
-primary_exchange AS bursa_efek,
-industry AS sektor_industri,
-category AS status_operasional
-FROM silver_company_profiles
-WHERE category = 'operating';
+    company_name AS perusahaan,
+    primary_ticker AS kode_saham,
+    primary_exchange AS bursa_efek,
+    industry AS sektor_industri,
+    category AS status_operasional
+FROM silver_company_profiles WHERE category = 'operating';
